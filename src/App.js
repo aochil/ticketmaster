d1a0796c47ffd51043524a1ffb4186aa8f538a50
aochil <azizbek.ochilov2@gmail.com>
2023-03-10 13:42:14 -0500

    starting point

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+
+  return (
+    <div data-testid="app">
+        <h1>Ticketmaster</h1>
+    </div>
+    
+  );
+}
+
+export { App };
