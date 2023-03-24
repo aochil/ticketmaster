0f496c50e8d03dde10c8852d0bbe8a39668a2abd
aochil <azizbek.ochilov2@gmail.com>
2023-03-24 16:57:18 -0400

    basic useEffect + api setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { get } from "lodash";
 import React, { useEffect, useState } from "react";
 
+const TM_KEY = process.env.REACT_APP_TM_KEY;
+
+const TM_API = `https://app.ticketmaster.com/discovery/v2/events?apikey=${TM_KEY}`;
 
 function App() {
+  const [eventList, setEventList] = useState([]);
+
+  useEffect(() => {
+    const getEvents = () => {
+      fetch(TM_API)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setEventList(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getEvents();
+  }, []);
 
   return (
     <div data-testid="app">
-        <h1>Ticketmaster</h1>
+      <h1>Ticketmaster</h1>
     </div>
-    
   );
 }
 
