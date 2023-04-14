d04885ef0cfa25f929b9d1c6eab54909c03e8429
Perom Peter Pen <officialpeterp@yahoo.com>
2023-04-14 13:21:15 -0700

    adds header/footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Home } from "./routes/Home";
 
 function App() {
 
   return (
-    <div data-testid="app">
-        <h1>Ticketmaster</h1>
-    </div>
-    
+    <BrowserRouter>
+      <Header/>
+      <div data-testid="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
