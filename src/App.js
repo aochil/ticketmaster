682091d6ebbce3b4bbdfd31bd78ce1e6d7069702
aochil <azizbek.ochilov2@gmail.com>
2023-07-02 13:59:24 -0400

    fixed import syntax, app builds

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import TicketmasterNavbar from "./components/TicketmasterNavbar.js";
 import Concerts from "./routes/Concerts.js";
 import Sports from "./routes/Sports.js";
