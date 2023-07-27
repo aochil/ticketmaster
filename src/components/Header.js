6e15863e6d2ae259a9cee1ff28f85ccdadb63b41
aochil <azizbek.ochilov2@gmail.com>
2023-07-26 20:03:19 -0400

    updated class -> className

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ import { SearchBar } from "./GetEvents";
 
 const Header = () => {
   return (
-    <nav id="navbar" class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
+    <nav id="navbar" className="navbar navbar-expand-md navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
         Ticketmaster
       </a>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarSupportedContent"
@@ -17,30 +17,30 @@ const Header = () => {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Concerts <span class="sr-only">(current)</span>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Concerts <span className="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Sports <span class="sr-only">(current)</span>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Sports <span className="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Arts & Theater <span class="sr-only">(current)</span>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Arts & Theater <span className="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item dropdown">
+            <li className="nav-item dropdown">
               <a
-                class="nav-link dropdown-toggle"
+                className="nav-link dropdown-toggle"
                 href="#"
                 id="navbarDropdown"
                 role="button"
@@ -50,37 +50,37 @@ const Header = () => {
               >
                 More
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
                   Family
                 </a>
-                <a class="dropdown-item" href="#">
+                <a className="dropdown-item" href="#">
                   Deals
                 </a>
-                <a class="dropdown-item" href="#">
+                <a className="dropdown-item" href="#">
                   Entertainment Guides
                 </a>
               </div>
             </li>
-            <div class="divider"></div>
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Sign In <span class="sr-only">(current)</span>
+            <div className="divider"></div>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Sign In <span className="sr-only">(current)</span>
               </a>
             </li>{" "}
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Sell <span class="sr-only">(current)</span>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Sell <span className="sr-only">(current)</span>
               </a>
             </li>{" "}
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Gift Cards <span class="sr-only">(current)</span>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Gift Cards <span className="sr-only">(current)</span>
               </a>
             </li>{" "}
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Help <span class="sr-only">(current)</span>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Help <span className="sr-only">(current)</span>
               </a>
             </li>
           </ul>
